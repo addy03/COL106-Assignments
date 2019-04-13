2159019d23e89976264459724e879e88117bcc19
Aditya Gupta <adityag1999@gmail.com>
2019-04-13 12:07:28 +0530

    Suffix Tree

--- Assgn5/src/suffix.java
+++ Assgn5/src/suffix.java
@@ -9,11 +9,11 @@ public class suffix
         root = new node(" ");
     }
 
-    public void AddElement(String s)
+    public void AddElement(String st)
     {
         if(root.child.first == null)
         {
-            node x = new node(s);
+            node x = new node(st);
             x.parent = root;
             root.child.AddNode(x);
         }
@@ -30,18 +30,18 @@ public class suffix
                 while(a != null)
                 {
                     int j = 0;
-
+                    System.out.println(a.elem.s + " " + a.elem.s.length());
                     String s1 = Character.toString(a.elem.s.charAt(j));
-                    String s2 = Character.toString(s.charAt(0));
+                    String s2 = Character.toString(st.charAt(0));
 //                    System.out.println(s1.equals(s2));
                     while(s1.equals(s2))
                     {
-                        s = s.substring(1);
+                        st = st.substring(1);
                         j += 1;
-                        if(s.length()>0 && a.elem.s.length()>j)
+                        if(st.length()>0 && a.elem.s.length()>j)
                         {
                             s1 = Character.toString(a.elem.s.charAt(j));
-                            s2 = Character.toString(s.charAt(0));
+                            s2 = Character.toString(st.charAt(0));
                         }
                         else
                         {
@@ -64,12 +64,12 @@ public class suffix
                 {
                     flag = 2;
                 }
-                else if(s.length() == 0)
+                else if(st.length() == 0)
                 {
                     f = a.elem;
                     flag = 1;
                 }
-                else if(a.elem.s.length() == pos+1)
+                else if(a.elem.s.length() == pos+1 && st.length() != 0)
                 {
                     f = a.elem;
                     flag = 0;
@@ -84,78 +84,55 @@ public class suffix
 //            System.out.println(f.s + "   !!!!!!!!!!!!!!!!!!!!!!!");
             if(flag == 2)
             {
-                node x = new node(s);
+                node x = new node(st);
                 x.parent = f;
                 f.child.AddNode(x);
             }
             else if(flag == 3 || flag == 1)
             {
                 node x;
-                if(s.length() > 0)
+                if(st.length() > 0)
                 {
-                    x = new node(s);
+                    x = new node(st);
                 }
                 else
                 {
-                    x = new node("#");
+                    x = new node( "#");
                 }
                 System.out.println(f.s.length() + " " + f.s);
-                node x2 = new node(f.s.substring(pos));
-                if(f.child.first != null)
+                node x2;
+                if(f.s.substring(pos).length() > 0)
                 {
-                    listNode m = f.child.first;
-                    while(m != null)
+                    x2 = new node(f.s.substring(pos));
+                    if(f.child.first != null)
                     {
-                        x2.child.AddNode(m.elem);
-                        m = m.next;
+                        listNode m = f.child.first;
+                        while(m != null)
+                        {
+                            x2.child.AddNode(m.elem);
+                            m = m.next;
+                        }
                     }
                 }
+                else
+                {
+                    x2 = null;
+                }
+
                 f.child.first = null;
-                f.child.AddNode(x2);
+                if(x2 != null)
+                {
+                    f.child.AddNode(x2);
+                    x2.parent = f;
+                }
                 f.s = f.s.substring(0,pos);
                 f.child.AddNode(x);
                 x.parent = f;
-                x2.parent = f;
             }
-
-//            if(pos > 0)
-//            {
-//                node x;
-//                if(s.length() > 0)
-//                {
-//                    x = new node(s);
-//                }
-//                else
-//                {
-//                    x = new node("#");
-//                }
-//                node x2 = new node(f.s.substring(pos));
-//                if(f.child.first != null)
-//                {
-//                    listNode m = f.child.first;
-//                    while(m != null)
-//                    {
-//                        x2.child.AddNode(m.elem);
-//                        m = m.next;
-//                    }
-//                }
-//                f.child.first = null;
-//                f.child.AddNode(x2);
-//                f.s = f.s.substring(0,pos);
-//                f.child.AddNode(x);
-//                x.parent = f;
-//                x2.parent = f;
-//            }
-//            else
-//            {
-//                node x = new node(s);
-//                f.child.AddNode(x);
-//                x.parent = f;
-//            }
         }
-        System.out.println("===================");
-        print_elem(root);
-        System.out.println("===================");
+//        System.out.println("===================");
+//        print_elem(root);
+//        System.out.println("===================");
     }
 
     public void print_elem(node a)
